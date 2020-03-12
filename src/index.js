a76c462648983c22ae98831b607ee3eff76f5de5
Christopher Henn <christopher@stripe.com>
2020-03-12 13:42:09 -0700

    Use pk from Elements Examples page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import App from "./App";
 import "./index.css";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_PK);
+const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
 ReactDOM.render(
   <Elements stripe={stripePromise}>
